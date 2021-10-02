a818cfdcabc4d0dbeb2f16a75d07783bf58007d5
arsalanhub <arsalanmohd237@gmail.com>
2021-10-02 21:57:00 +0530

    updates

--- src/index.js
+++ src/index.js
@@ -5,3 +5,11 @@ import './index.css';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './index.css';
+import App from './App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
